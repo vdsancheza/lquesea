acb8fcb1be414c1ead0d27c164ea99116a853c1d
vdsancheza <vdsancheza@robotprogramador.com>
2017-06-26 22:29:15 -0400

    added module for firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
@@ -23,6 +24,18 @@ import { routes }   from './app.route';
 import { CategoryActions, QuestionActions, TagActions } from './state-management/actions';
 import { CategoryEffects, QuestionEffects, TagEffects } from './state-management/effects';
 import { default as reducer } from './state-management/state/app.store';
+
+//firebase
+import { AngularFireModule } from 'angularfire2';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDYnAsJXasESdwZWKD3f_P6msBsqDzJwXM",
+    authDomain: "lquesea-a798c.firebaseapp.com",
+    databaseURL: "https://lquesea-a798c.firebaseio.com",
+    projectId: "lquesea-a798c",
+    storageBucket: "lquesea-a798c.appspot.com",
+    messagingSenderId: "313133868861"
+  };
 
 @NgModule({
   declarations: [
@@ -49,7 +62,8 @@ import { default as reducer } from './state-management/state/app.store';
     EffectsModule.run(TagEffects),
     StoreDevtoolsModule.instrumentOnlyWithExtension({
       maxAge: 20
-    })
+    }),
+    AngularFireModule.initializeApp(firebaseConfig),
     
   ],
   providers: [
